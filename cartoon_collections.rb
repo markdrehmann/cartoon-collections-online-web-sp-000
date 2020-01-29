25ace48cc26bace0f286b5b960d392424ed6d78a
Mark Drehmann <markdrehmann@gmail.com>
2020-01-29 00:53:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,10 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_calls)
+  long_calls.include? do |call|
+    call.length >= 4
+  end
 end
 
 def find_the_cheese# code an argument here
